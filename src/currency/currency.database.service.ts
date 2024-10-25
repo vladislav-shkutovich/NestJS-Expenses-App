4001ee032471e1967f8a9d2cdd354cd4c6a7373d
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-10-25 11:40:49 +0300

    feat: currency contoller->service->db-service interface

--- src/currency/currency.database.service.ts
+++ src/currency/currency.database.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
-import { Model } from 'mongoose'
+import { Model, Types } from 'mongoose'
 
 import { CURRENCY_MODEL } from '../common/constants/database.constants'
 import type { Currency } from './schemas/currency.schema'
@@ -10,4 +10,13 @@ export class CurrencyDatabaseService {
   constructor(
     @InjectModel(CURRENCY_MODEL) private currencyModel: Model<Currency>,
   ) {}
+
+  async getAllCurrencies(): Promise<Currency[]> {
+    return [] as Currency[]
+  }
+
+  async getCurrencyById(id: Types.ObjectId): Promise<Currency> {
+    console.error('mock id', id)
+    return {} as Currency
+  }
 }
--- src/currency/currency.service.ts
+++ src/currency/currency.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@nestjs/common'
 
+import { Types } from 'mongoose'
 import { CurrencyDatabaseService } from './currency.database.service'
+import type { Currency } from './schemas/currency.schema'
 
 @Injectable()
 export class CurrencyService {
   constructor(
     private readonly currencyDatabaseService: CurrencyDatabaseService,
   ) {}
+
+  async getAllCurrencies(): Promise<Currency[]> {
+    return await this.currencyDatabaseService.getAllCurrencies()
+  }
+
+  async getCurrencyById(id: Types.ObjectId): Promise<Currency> {
+    return await this.currencyDatabaseService.getCurrencyById(id)
+  }
 }
