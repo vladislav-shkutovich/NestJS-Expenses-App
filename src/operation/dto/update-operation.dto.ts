0a0298d376b8bd0163f3b916c5ea4f4536a511f5
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-11-27 16:34:34 +0300

    fix: update operation dto

--- src/operation/dto/update-operation.dto.ts
+++ src/operation/dto/update-operation.dto.ts
@@ -1,5 +1,33 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { Type } from 'class-transformer'
+import {
+  IsDate,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator'
+import { Types } from 'mongoose'
 
-import { CreateOperationDto } from './create-operation.dto'
+import { IsValidObjectId } from '../../common/decorators/is-valid-objectid.decorator'
+import { TransformStringToObjectId } from '../../common/decorators/transform-string-to-objectid.decorator'
 
-export class UpdateOperationDto extends PartialType(CreateOperationDto) {}
+export class UpdateOperationDto {
+  @TransformStringToObjectId()
+  @IsValidObjectId()
+  @IsOptional()
+  categoryId?: Types.ObjectId
+
+  @IsNumber()
+  @IsOptional()
+  amount?: number
+
+  @IsDate()
+  @Type(() => Date)
+  @IsOptional()
+  date?: Date
+
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  description?: string
+}
