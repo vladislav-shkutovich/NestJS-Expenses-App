168c61e004f6b145e9b580c95475259897459a19
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-11-28 17:20:36 +0300

    fix: throw not found error above the handle db error in update category

--- src/category/category.database.service.ts
+++ src/category/category.database.service.ts
@@ -66,6 +66,9 @@ export class CategoryDatabaseService {
 
       return updatedCategory
     } catch (error) {
+      if (error instanceof NotFoundError) {
+        throw error
+      }
       this.handleDatabaseError(error)
     }
   }
