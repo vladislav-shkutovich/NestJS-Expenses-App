34cdb90e605c1ca338e7161a4f2db5c33220e70a
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2024-12-03 17:02:08 +0300

    feat: add transaction service for a global usage

---
+++ src/common/services/transaction.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common'
+import { InjectConnection } from '@nestjs/mongoose'
+import { ClientSession, Connection } from 'mongoose'
+
+@Injectable()
+export class TransactionService {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  async executeInTransaction<T>(
+    transactionCallback: (session: ClientSession) => Promise<T>,
+  ): Promise<T> {
+    const session = await this.connection.startSession()
+    session.startTransaction()
+    try {
+      const result = await transactionCallback(session)
+      await session.commitTransaction()
+      console.log('Transaction committed successfully')
+      return result
+    } catch (error) {
+      await session.abortTransaction()
+      console.error('Transaction aborted. error: ', error)
+      throw error
+    } finally {
+      session.endSession()
+    }
+  }
+}
