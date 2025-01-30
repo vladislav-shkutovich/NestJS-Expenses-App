629874f443e37244999a250e0a95786c91bab20a
vladislav-shkutovich <vladislav.shkutovich@gmail.com>
2025-01-30 19:00:27 +0300

    chore: add migration for exchange rate main indexes

---
+++ migrations/2025.01.30T15.27.06.exchange-rates-create-main-indexes.mjs
@@ -0,0 +1,27 @@
+const EXCHANGE_RATES_INDEX_NAMES = {
+  VALIDTO: 'validTo_1',
+  BASECURRENCY_TARGETCURRENCY_VALIDFROM_VALIDTO:
+    'baseCurrency_1_targetCurrency_1_validFrom_1_validTo_1',
+}
+
+export async function up({ context }) {
+  const collection = context.collection('exchangerates')
+  await collection.createIndex(
+    { validTo: 1 },
+    { name: EXCHANGE_RATES_INDEX_NAMES.VALIDTO },
+  )
+  await collection.createIndex(
+    { baseCurrency: 1, targetCurrency: 1, validFrom: 1, validTo: 1 },
+    {
+      name: EXCHANGE_RATES_INDEX_NAMES.BASECURRENCY_TARGETCURRENCY_VALIDFROM_VALIDTO,
+    },
+  )
+}
+
+export async function down({ context }) {
+  const collection = context.collection('exchangerates')
+  await collection.dropIndex(
+    EXCHANGE_RATES_INDEX_NAMES.BASECURRENCY_TARGETCURRENCY_VALIDFROM_VALIDTO,
+  )
+  await collection.dropIndex(EXCHANGE_RATES_INDEX_NAMES.VALIDTO)
+}
